3ca436cdb77f1cf917f7f6d7b0fdf4c5fcd98970
Ayiks <cliffysarp@gmail.com>
2022-03-26 23:11:24 +0000

    changes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,23 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIwS4SarfTLwxbj-NkmBz_7Ky6Tl3_TcA",
-  authDomain: "let-s-accommodate.firebaseapp.com",
-  projectId: "let-s-accommodate",
-  storageBucket: "let-s-accommodate.appspot.com",
-  messagingSenderId: "642825766952",
-  appId: "1:642825766952:web:bfc57c1ff5e1bb4b6c8d1d",
-  measurementId: "G-Q3Z4QVCQRJ",
+  apiKey: "AIzaSyC3kmNGvASLImX4IFKo2rfvRZB0TPsjPw4",
+  authDomain: "project-pull-up-2022-03-04.firebaseapp.com",
+  projectId: "project-pull-up-2022-03-04",
+  storageBucket: "project-pull-up-2022-03-04.appspot.com",
+  messagingSenderId: "540470947208",
+  appId: "1:540470947208:web:b333281f30d19f5e57de3d"
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-
-// npm install firebase @firebase/firestore-types @firebase/app-types @firebase/database-types
